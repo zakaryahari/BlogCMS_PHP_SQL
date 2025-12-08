c785971353845ead25da0be98b8b567a4cdc1ca9
zakarya hari <zakariahari42@gmail.com>
2025-12-08 23:01:28 +0100

    Corriger des error de syntax.

--- classes/user.php
+++ classes/user.php
@@ -13,7 +13,7 @@
         }
 
         public function Add_new_user(){
-            $sql = "INSERT INTO utilisateur VALUES (:username,:email,:mod_de_pass,:date_inscription,:role)"
+            $sql = "INSERT INTO utilisateur VALUES (:username,:email,:mod_de_pass,:date_inscription,:role)";
 
             $query = $this->connection->prepare($sql);
 
@@ -21,8 +21,8 @@
             $this->email = htmlspecialchars(strip_tags($this->email));
 
             $query->bindParam(":username",$this->username);
-            $query->bindParam(":email",$this->username);
-            $query->bindParam(":mod_de_pass",$this->username);
+            $query->bindParam(":email",$this->email);
+            $query->bindParam(":mod_de_pass",$this->mod_de_pass);
             $query->bindParam(":date_inscription",$this->date_inscription);
             $query->bindParam(":role",$this->role);
 
