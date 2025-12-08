0707bdbdda6c3bf5f1766a661c531729a9459315
zakarya hari <zakariahari42@gmail.com>
2025-12-09 00:22:31 +0100

    Création de la classe categorie et implémentation de la méthode d'ajout d'une categorie.

---
+++ classes/categorie.php
@@ -0,0 +1,29 @@
+<?php 
+    class categorie {
+        private $connection;
+
+        public $id_categorie;
+        public $libelle;
+        public $description;
+
+        public function __construct($connection){
+            $this->connection = $connection;
+        }
+
+        public function Add_new_categorie(){
+            $sql = "INSERT INTO utilisateur VALUES (:id_categorie,:libelle,:description)";
+
+            $query = $this->connection->prepare($sql);
+
+            $query->bindParam(":id_categorie",$this->id_categorie);
+            $query->bindParam(":libelle",$this->libelle);
+            $query->bindParam(":description",$this->description);
+
+
+            if($query->execute()) {
+                return true;
+            }
+            return false;
+        }
+    }
+?>
