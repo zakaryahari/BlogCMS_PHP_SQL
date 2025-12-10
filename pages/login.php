0fa46579d12e760dc4a46073ae4b87721f5df3b7
zakarya hari <zakariahari42@gmail.com>
2025-12-10 14:55:14 +0100

    Gérer la redirection après connexion selon le rôle (admin ou utilisateur).

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,35 @@
+<?php 
+  require_once '../classes/database.php';
+  require_once '../classes/user.php';
+
+  if (isset($_POST['submit'])) {
+    $connection = new Database();
+    $db = $connection->getConnection();
+
+    $user = new user($db);
+
+    $username_input = $_POST['username'];
+    $password_input = $_POST['password'];
+
+    $check = $user->login($username_input , $password_input);
+    
+    if ($check == false) {
+        echo "Username dosen't exists!";
+    } 
+    elseif($check != false) {
+
+      if ($check['role'] == 'admin') {
+        echo "hello admin";
+        header('Location: ../index.html');
+        exit();
+      }
+      else {
+        echo "hello user";
+      }
+    }
+
+  }
+?>
 <!DOCTYPE html>
 <html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
   <head>
@@ -20,18 +52,19 @@
           <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
             <div class="w-full">
               <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">Login</h1>
-              <label class="block text-sm">
-                <span class="text-gray-700 dark:text-gray-400">Email</span>
-                <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" />
-              </label>
-              <label class="block mt-4 text-sm">
-                <span class="text-gray-700 dark:text-gray-400">Password</span>
-                <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="***************" type="password" />
-              </label>
-
+              <form action="" method="POST">
+                <label class="block text-sm">
+                  <span class="text-gray-700 dark:text-gray-400">Username</span>
+                  <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" type="text" name="username"/>
+                </label>
+                <label class="block mt-4 text-sm">
+                  <span class="text-gray-700 dark:text-gray-400">Password</span>
+                  <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="***************" type="password" name="password"/>
+                </label>
+                <button type="submit" name="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"> Log in </button>
+              </form>
               <!-- You should use a button here, as the anchor is only used for the example  -->
               <!-- <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="../index.html"> Log in </a> -->
-              <button type="submit" name="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"> Log in </button>
 
 
               <hr class="my-8" />
