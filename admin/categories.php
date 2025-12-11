5e8f179cbbb44d94775ccb7996199b4f06dca44d
zakarya hari <zakariahari42@gmail.com>
2025-12-11 22:38:15 +0100

    Finaliser la parter de ajouter une categorie.

--- admin/categories.php
+++ admin/categories.php
@@ -2,6 +2,7 @@
 
       require_once '../classes/database.php';
       require_once '../classes/user.php';
+      require_once '../classes/categorie.php';
 
       $connection = new Database();
       $db = $connection->getconnection();
@@ -14,7 +15,7 @@
         }
       }
       else {
-          header("Location: ../pages/login.php");
+          header("Location: ../login.php");
           exit();
       }
 
@@ -23,7 +24,23 @@
       $stmt = $db->query($sql_All_Categories);
       $All_Categories = $stmt->fetchAll();
 
+      if (isset($_POST['submit_category'])) {
+        $categorie = new categorie($db);
 
+        $categorie->libelle = $_POST['libelle'];
+        $categorie->description = $_POST['description'];
+
+        // $categorie->Add_new_categorie();
+        if ($categorie->Add_new_categorie()) {
+            echo "categorie created seccessfuly !!";
+            header("Location: categories.php"); 
+            exit();
+        }
+        else {
+            echo "there was a error creating this categorie";
+        }
+
+      }
       
 ?>
 
@@ -206,7 +223,49 @@
                                     <?php endforeach;?>
                                 </tbody>
                             </table>
-                            
+                            <div id="categoryModal" class="fixed inset-0 z-30 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center hidden">
+                                <div class="w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl" role="dialog" id="modal">
+                                    
+                                    <header class="flex justify-end">
+                                        <button class="inline-flex items-center justify-center w-6 h-6 text-gray-400 transition-colors duration-150 rounded dark:hover:text-gray-200 hover: hover:text-gray-700" aria-label="close" onclick="closeModal()">
+                                            <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" role="img" aria-hidden="true">
+                                                <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path>
+                                            </svg>
+                                        </button>
+                                    </header>
+
+                                    <div class="mt-4 mb-6">
+                                        <p class="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300">
+                                            Category Management
+                                        </p>
+                                        
+                                        <form action="categories.php" method="POST" class="form_container">
+                                            <input type="hidden" name="id_categorie" id="modal_id_categorie">
+
+                                            <label class="block text-sm">
+                                                <span class="text-gray-700 dark:text-gray-400">Category Name</span>
+                                                <input type="text" name="libelle" id="modal_libelle" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="e.g. Technology" required 
+                                                />
+                                            </label>
+
+                                            <label class="block mt-4 text-sm">
+                                                <span class="text-gray-700 dark:text-gray-400">Description</span>
+                                                <textarea name="description" id="modal_description" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-textarea focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray" rows="3" placeholder="Short description of the category."
+                                                ></textarea>
+                                            </label>
+
+                                            <footer class="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800">
+                                                <button type="button" id="form_cancel_btn" class="w-full px-5 py-3 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray">
+                                                    Cancel
+                                                </button>
+                                                <button type="submit" name="submit_category" id="form_save_btn" class="w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+                                                    Save
+                                                </button>
+                                            </footer>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
