82bf6cde3245a5d8c4d0c0e46d6313740f32fb6a
zakarya hari <zakariahari42@gmail.com>
2025-12-12 01:30:51 +0100

    Ajouter le script pour suprimer une categorie.

--- admin/categories.php
+++ admin/categories.php
@@ -23,9 +23,9 @@
 
       $stmt = $db->query($sql_All_Categories);
       $All_Categories = $stmt->fetchAll();
-
+      
+      $categorie = new categorie($db);
       if (isset($_POST['submit_category'])) {
-        $categorie = new categorie($db);
 
         $categorie->id_categorie = $_POST['id_categorie'];
         $categorie->libelle = $_POST['libelle'];
@@ -55,6 +55,19 @@
 
       }
       
+      if (isset($_GET['action']) && isset($_GET['id'])) {
+        $categorie->id_categorie = $_GET['id'];
+        // if (confirm(are you sure you whant to delete this !!!)) {            
+            if ($categorie->Delete_categorie() != false) {
+                echo "categorie deleted seccessfuly!!";
+                header("Location: categories.php"); 
+                exit();
+            }
+            else{
+                echo "there was a error deleting this categorie";
+            }
+        }
+    //   }
 ?>
 
 <!DOCTYPE html>
@@ -225,11 +238,11 @@
                                                         <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
                                                     </svg>
                                                 </button>
-                                                <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete" data-id_categorie="<?php echo $row['id_categorie']; ?>">
+                                                <a class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" onclick="return confirm('Are you sure you want to delete this category?');" aria-label="Delete" href="categories.php?action=delete&id=<?php echo $row['id_categorie']; ?>">
                                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                                         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                                                     </svg>
-                                                </button>
+                                                </a>
                                             </div>
                                         </td>
                                     </tr>
