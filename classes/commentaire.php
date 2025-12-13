db2c181ee2a4c3cb27652458d52a9f9d19242e9e
zakarya hari <zakariahari42@gmail.com>
2025-12-13 18:42:07 +0100

    modify la methode qui a ajouter un commentaire.

--- classes/commentaire.php
+++ classes/commentaire.php
@@ -17,24 +17,22 @@
         public function Add_new_commentaire(){
 
             $sql = "INSERT INTO commentaire (contenu_commentaire, date_commentaire, username, id_article, email, status) 
-                    VALUES (:contenu_commentaire, :date_commentaire, :username, :id_article, :email, :status)";
+                    VALUES (:contenu_commentaire, NOW(), :username, :id_article, :email, 'approved')";
 
             $query = $this->connection->prepare($sql);
 
             $this->contenu_commentaire = htmlspecialchars(strip_tags($this->contenu_commentaire));
 
-            $this->username = htmlspecialchars(strip_tags($this->username));
+            if (empty($this->username)) {
+                $this->username = null;
+            } else {
+                $this->username = htmlspecialchars(strip_tags($this->username));
+            }
             
-            $this->email = htmlspecialchars(strip_tags($this->email));
-            
-            $this->status = htmlspecialchars(strip_tags($this->status));
-
             $query->bindParam(":contenu_commentaire", $this->contenu_commentaire);
-            $query->bindParam(":date_commentaire", $this->date_commentaire);
             $query->bindParam(":username", $this->username);
             $query->bindParam(":id_article", $this->id_article);
             $query->bindParam(":email", $this->email);
-            $query->bindParam(":status", $this->status);
 
             if($query->execute()) {
                 return true;
@@ -105,5 +103,7 @@
             }
             return [];
         }
+
+        
     }
 ?>
